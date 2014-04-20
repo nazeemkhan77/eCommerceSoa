dab9b7643e544cea0e2080c6206edfec508c7b0e
nazeemkhan77 <nazeem.khan@gmail.com>
2014-04-20 15:34:05 -0500

    Committing the missed files

---
+++ eCommerceSoa/Domain/Orders.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain
+{
+    class Orders
+    {
+    }
+}
---
+++ eCommerceSoa/Domain/Transaction/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain.Master.Customer;
+using Domain.Master.Product;
+
+namespace Domain.Transaction
+{
+    public class ShoppingCart
+    {
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
